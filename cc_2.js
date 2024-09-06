6cc44aa4a9cd5d71edee4074ea574f8a4101b8f9
tamarabush <tamara.abush14@outlook.com>
2024-09-06 11:04:49 -0400

    Initial logic for calculating tip using ternary operator.

---
+++ cc_2.js
@@ -0,0 +1,11 @@
+// TASK 1 - Calculate Tip:
+
+let bill = 250;
+
+let tip = (bill >= 50 && bill <= 300) ? (bill * 0.15) : (bill * 0.20); 
+// if true 250 >= 50 and <= 300, than we use a 15% tip. if false we use 20%.
+
+console.log(tip); //Output = 37.5 
+
+
+
