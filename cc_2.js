29d7f706fb62b62db1f1b11c18ed958227ba60a6
tamarabush <tamara.abush14@outlook.com>
2024-09-06 11:35:05 -0400

    Create and test calculateTip function

--- cc_2.js
+++ cc_2.js
@@ -15,3 +15,18 @@ let totalAmount = bill + tip; // Output = 287.5 (sum of bill and tip)
 
 console.log(`The bill is $${bill}, the tip is $${tip}, and the total value is $${totalAmount}.`); 
 //The bill is $250, the tip is $37.5, and the total value is $287.5.
+
+
+
+// TASK 3 - Create a Function and Testing:
+
+function calculateTip(bill) { 
+    return (bill >= 50 && bill <= 300) ? (bill * 0.15) : (bill * 0.20);
+}  //Used function declaration by using bill as the input and returned the calculated tip  
+
+let testBill = 100; 
+
+let testTip = calculateTip(testBill); //applied function declaration to testbill
+
+console.log(`The bill is $${testBill} and the tip is $${testTip}.`); //The bill is $100 and the tip is $15.
+
